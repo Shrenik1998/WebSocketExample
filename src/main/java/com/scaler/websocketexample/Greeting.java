a5005b9c05001278c66bf777634b2840b2d26ca4
Shrenik <shrenikhalyal98@gmail.com>
2024-10-29 10:18:20 +0530

    websocket example

---
+++ src/main/java/com/scaler/websocketexample/Greeting.java
@@ -0,0 +1,13 @@
+package com.scaler.websocketexample;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Greeting {
+    private String content;
+}
---
+++ src/main/java/com/scaler/websocketexample/HelloMessage.java
@@ -0,0 +1,10 @@
+package com.scaler.websocketexample;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloMessage {
+    private String name;
+}
